209c9e4fd3692bb75dfd5138ef98af4c1c8b1e08
krutoydev <starkmarvel074@gmail.com>
2022-03-19 14:14:57 +0500

    some changes to commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="nav bg-gray-100 py-2 shadow-md">
-      <div class="nav flex justify-between items-center px-5">
-          <router-link to="/" class=" text-black font-bold uppercase">shopping</router-link>
-          <router-link to="/men" class=" text-black font-bold uppercase">men</router-link>
-          <router-link to="/women" class=" text-black font-bold uppercase">women</router-link>
-          <router-link to="/kids" class=" text-black font-bold uppercase">kids</router-link>
+  <div class="nav   shadow-md">
+      <div class="nav flex py-10 items-center px-5 flex-col h-screen bg-black pt-20">
+          <router-link to="/" class=" text-white font-bold uppercase text-3xl">shopping</router-link>
+          <router-link to="/men" class=" text-white font-bold uppercase py-20 text-3xl">men</router-link>
+          <router-link to="/women" class=" text-white font-bold uppercase text-3xl">women</router-link>
+          <router-link to="/kids" class=" text-white font-bold uppercase py-20 text-3xl">kids</router-link>
           
       </div>
   </div>
 </template>
 
 <script>
+import {  } from '@heroicons/vue/solid'
 export default {
-
+  components:{
+    
+  }
 }
 </script>
 
